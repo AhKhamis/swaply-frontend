dbf1e49c4cd7c6da3dc89e79d2a3299175d25de3
AHMED <7mody.stacks@gmail.com>
2026-09-13 20:19:04 +0300

    Add swap service

---
+++ src/services/swapService.js
@@ -0,0 +1,22 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/swaps`;
+
+const getSwaps = async () => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
+
+  const res = await fetch(BASE_URL, config);
+  const data = await res.json();
+
+  if (data.err) {
+    throw new Error(data.err);
+  }
+
+  return data.swaps;
+};
+
+export {
+  getSwaps,
+};
